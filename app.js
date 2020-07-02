f75b12b14f73c3f0faa001b347d9c707d4e29cd5
Rupesh Chandra Mohanty <43889891+rupeshmohanty@users.noreply.github.com>
2020-07-01 22:46:38 -0700

    Files updated!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
 const { restart } = require('nodemon');
 const { eventNames } = require('./models/events');
 const Event = require('./models/events');
@@ -18,6 +19,7 @@ app.set('view engine','ejs');
 
 //Middleware
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended : false }));
 
 //Port no
@@ -29,7 +31,13 @@ app.get('/', async(req,res) => {
     res.render('index',{ events: events });
 });
 
-//add event page
+//edit-event page
+app.get('/edit-event/:id', async(req,res) => {
+    const event = await Event.findById(req.params.id);
+    res.render('edit-event', { event: event });
+});
+
+//add-event page
 app.get('/add-event', (req,res) => {
     res.render('add-event',{ event: new Event() });
 });
@@ -41,6 +49,7 @@ app.get('/:id', async(req,res) => {
     res.render('event-details', { event: event });
 });
 
+//POST request to add an event
 app.post('/add-event', async(req,res) => {
     let event = new Event();
     event.title = req.body.title;
@@ -54,6 +63,28 @@ app.post('/add-event', async(req,res) => {
         res.render('add-event', { event: event });
     }
 
+});
+
+//delete an event
+app.delete('/:id', async(req,res) => {
+    await Event.findByIdAndDelete(req.params.id);
+    res.redirect('/');
+});
+
+//PUT request to edit an event
+app.put('/:id', async(req,res) => {
+    req.event = await Event.findById(req.params.id);
+    let event = req.event;
+    event.title = req.body.title;
+    event.description = req.body.description;
+    event.event_date = req.body.event_date;
+
+    try{
+        event = await event.save();
+        res.redirect(`${event.id}`);
+    } catch(err) {
+        res.render('/edit-event', { event: event });
+    }
 });
 
 
